dfb4d59cb0e1611fabbde89148275586674ed827
yeeeeyaaaa <yeeeeyaaaa@gmail.com>
2015-12-14 21:59:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+   create : passport-domoticon
+   create : passport-domoticon/package.json
+   create : passport-domoticon/app.js
+   create : passport-domoticon/public
+   create : passport-domoticon/public/javascripts
+   create : passport-domoticon/public/stylesheets
+   create : passport-domoticon/public/stylesheets/style.css
+   create : passport-domoticon/routes
+   create : passport-domoticon/routes/index.js
+   create : passport-domoticon/routes/users.js
+   create : passport-domoticon/views
+   create : passport-domoticon/views/index.jade
+   create : passport-domoticon/views/layout.jade
+   create : passport-domoticon/views/error.jade
+   create : passport-domoticon/bin
+   create : passport-domoticon/bin/www
+
+   install dependencies:
+     $ cd passport-domoticon && npm install
+
+   run the app:
+     $ DEBUG=passport-domoticon:* npm start
+
+   create : passport-domoticon/public/images
---
+++ routes/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
+
+
+router.get('/', function(req, res) {
+    var pluginList = [];
+    pluginList.push({
+        id: "1",
+        name: "Arduino",
+        icon: "arduino.jpg"
+    });
+
+    res.render('index', {
+        user: req.user,
+        title: 'Domoticon Home',
+        plugins: pluginList
+    });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', {
+        title: 'Domoticon Register'
+    });
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({
+        username: req.body.username
+    }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render("register", {
+                info: "Sorry. That username already exists. Try again."
+            });
+        }
+
+        passport.authenticate('local')(req, res, function() {
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', {
+        user: req.user,
+        title: 'Domoticon Login'
+    });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/ping', function(req, res) {
+    res.status(200).send("pong!");
+});
+
+module.exports = router;
