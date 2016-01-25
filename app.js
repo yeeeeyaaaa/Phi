9144d8f35cd886f1b14527d550bd5394685c94f8
mbu <mbusquets@meteo.cat>
2016-01-25 12:43:45 +0100

    new library to connect Raspberry to Orvibo sockets via wifi

--- app.js
+++ app.js
@@ -15,6 +15,53 @@ var pluginHelper = require('utils');
 
 var app = express();
 
+var Orvibo = require("lib/orvibo.js")
+var o = new Orvibo()
+
+
+// We've listened, and now we're ready to go.
+o.on("ready", function() {
+  timer1 = setInterval(function() { // Set up a timer to search for sockets every second until found
+    o.discover()
+  }, 1000)
+})
+
+// A device has been found and added to our list of devices
+o.on("deviceadded", function(device) {
+  clearInterval(timer1) // Clear our first timer, as we've found at least one socket
+  o.discover() // Ask around again, just in case we missed something
+  timer2[device.macAddress] = setInterval(function() { // Set up a new timer for subscribing to this device. Repeat until we get confirmation of subscription
+    o.subscribe(device)
+  }, 1000)
+})
+
+// We've asked to subscribe (control) a device, and now we've had a response.
+// Next, we will query the device for its name and such
+o.on("subscribed", function(device) {
+  clearInterval(timer2[device.macAddress]) // Stop the second subscribe timer for this device
+  timer3[device.macAddress] = setInterval(function() { // Set up another timer, this time for querying
+    o.query({
+      device: device, // Query the device we just subscribed to
+      table: "04" // See PROTOCOL.md for info. "04" = Device info, "03" = Timing info
+    })
+  }, 1000)
+})
+
+// Our device has responded to our query request
+o.on("queried", function(device, table) {
+  clearInterval(timer3[device.macAddress]) // Stop the query timer
+  if (device.type == "Socket") { // If this is a socket
+    timer4[device.macAddress] = setInterval(function() { // Set up another timer that toggles the state of the unit every 5 seconds
+      o.setState({
+        device: device,
+        state: !device.state // The inverse of the current state
+      })
+    }, 5000)
+  } else if (device.type == "AllOne") { // If we've got an AllOne instead
+    o.enterLearningMode(device) // Put it into learning mode
+  }
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -37,14 +84,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*pluginHelper.getPluginList(function(err, plugins) {
-    app.locals.plugins = plugins;
-    app.set('plugins', plugins);
-
-    // 404 Not found
-    app.all('*', Routes.notFound);
-
-});*/
 var pluginList = [];
 pluginList.push({
     id: "1",
